9806f61e6eb95602505d3f3513ef7a6346762851
lukasjoho <social@lukashoppe.com>
2021-08-06 20:35:01 +0200

    Create routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-
+import Link from 'next/link';
 export default function Home() {
-	return <div></div>;
+	return (
+		<div>
+			<Link
+				href={{
+					pathname: '/[username]',
+					query: { username: 'jeffd23' },
+				}}
+			>
+				<a href=''>Jeffs Profil</a>
+			</Link>
+		</div>
+	);
 }
