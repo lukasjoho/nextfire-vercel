7cab943f4a104b32ab218f4b0c707d8d9b5bfd1d
lukasjoho <social@lukashoppe.com>
2021-08-09 00:24:14 +0200

    Increase limit to 10

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import PostFeed from '../components/PostFeed';
+import Metatags from '../components/Metatags';
 import Loader from '../components/Loader';
 import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
 import { useState } from 'react';
 
 // Max post to query per page
-const LIMIT = 1;
+const LIMIT = 10;
 
 export async function getServerSideProps(context) {
 	const postsQuery = firestore.collectionGroup('posts').where('published', '==', true).orderBy('createdAt', 'desc').limit(LIMIT);
@@ -22,7 +23,8 @@ export default function Home(props) {
 	const [loading, setLoading] = useState(false);
 
 	const [postsEnd, setPostsEnd] = useState(false);
-	console.log(props.posts);
+
+	// Get next page in pagination query
 	const getMorePosts = async () => {
 		setLoading(true);
 		const last = posts[posts.length - 1];
@@ -35,15 +37,32 @@ export default function Home(props) {
 
 		setPosts(posts.concat(newPosts));
 		setLoading(false);
+
+		if (newPosts.length < LIMIT) {
+			setPostsEnd(true);
+		}
 	};
 
 	return (
 		<main>
+			<Metatags title='Home Page' description='Get the latest posts on our site' />
+
+			<div className='card card-info'>
+				<h2>💡 Next.js + Firebase - The Full Course</h2>
+				<p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
+				<p>
+					Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine
+					optimized.
+				</p>
+			</div>
+
 			<PostFeed posts={posts} />
+
 			{!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
 			<Loader show={loading} />
+
 			{postsEnd && 'You have reached the end!'}
-			test
 		</main>
 	);
 }
