968ba14c1cf748aac00ba3f7e7242165b00f01cb
lukasjoho <social@lukashoppe.com>
2021-08-08 21:17:59 +0200

    Pre postfeed on home

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,52 @@ import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import Loader from '../components/Loader';
 import toast from 'react-hot-toast';
-export default function Home() {
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
+
+import { useState } from 'react';
+
+import PostFeed from '../components/PostFeed';
+
+const LIMIT = 1;
+export async function getServerSideProps(context) {
+	const postsQuery = firestore.collectionGroup('posts').where('published', '==', true).orderBy('createdAt', 'desc').limit(LIMIT);
+
+	const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+	return {
+		props: { posts },
+	};
+}
+
+export default function Home(props) {
+	const [posts, setPosts] = useState(props.posts);
+	const [loading, setLoading] = useState(false);
+
+	const [postsEnd, setPostsEnd] = useState(false);
+
+	const getMorePosts = async () => {
+		setLoading(true);
+		const last = posts[posts.length - 1];
+
+		const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+		const query = firestore.collectionGroup('posts').where('published', '==', true).orderBy('createdAt', 'desc').startAfter(cursor).limit(LIMIT);
+
+		const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+		setPosts(posts.concat(newPosts));
+		setLoading(false);
+
+		if (newPosts.length < LIMIT) {
+			setPostsEnd(true);
+		}
+	};
 	return (
-		<div>
-			<button onClick={() => toast.success('Hello Toast!')}>Toast Meee </button>
-		</div>
+		<main>
+			<PostFeed posts={posts} />
+			{!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+			<Loader show={loading} />
+			{postsEnd && 'You have reached the end'}
+		</main>
 	);
 }
