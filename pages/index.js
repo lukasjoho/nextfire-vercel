371d88170226f11f1af59ea1eee14cda728c37c0
lukasjoho <social@lukashoppe.com>
2021-08-08 15:23:25 +0200

    Add toaster and navbar

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
 export default function Home() {
 	return (
 		<div>
-			<Loader show />
+			<button onClick={() => toast.success('Hello Toast!')}>Toast Meee </button>
 		</div>
 	);
 }
